2a125089aaf5767291db8730820b7306da1d0074
Andrew Kirkpatrick <info@andrew-kirkpatrick.com>
2013-04-18 18:08:44 +0100

    Moved component.json to web root, Added routes to Silex application

--- web/index.php
+++ web/index.php
@@ -1,3 +1,24 @@
 <?php
 
-echo 'Hello world!';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request as Request;
+use Symfony\Component\HttpFoundation\Response as Response;
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+$app->get('/', function(Request $response, Response $response) {
+	return 'Hello world';
+});
+
+$app->get('/login', function(Request $response, Response $response) {
+	return 'Login';
+});
+
+$app->post('/login', function(Request $response, Response $response) {
+	return 'POST login';
+});
+
+$app->run();
