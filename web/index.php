5e4fb56914527a0d300aad588eccaabe7a2202a6
Andrew Kirkpatrick <info@andrew-kirkpatrick.com>
2013-04-23 13:08:25 +0100

    Routing now works

--- web/index.php
+++ web/index.php
@@ -2,23 +2,55 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// Import declarations
 use Symfony\Component\HttpFoundation\Request as Request;
 use Symfony\Component\HttpFoundation\Response as Response;
 
+// Start Silex
 $app = new Silex\Application();
 
+// Configuration
 $app['debug'] = true;
 
-$app->get('/', function(Request $response, Response $response) {
-	return 'Hello world';
+$baseUrl = '/localhost/cricket/web/';
+// Service providers
+
+
+$loggedIn = true;
+
+$app->get('', function() use ($app, $loggedIn) {
+	if (! $loggedIn) {
+		return $app->redirect('login');
+	}
+	return 'Dashboard';
 });
 
-$app->get('/login', function(Request $response, Response $response) {
+$app->get('login', function() {
 	return 'Login';
 });
 
-$app->post('/login', function(Request $response, Response $response) {
+$app->post('login', function(Request $request) {
 	return 'POST login';
+	
+	$something = 'Hallo';
+	$something = $app->escape($something);
+	
+	return $app->redirect('/');
 });
 
+$app->get('/summary', function(Request $request) {
+	return 'Summary';
+});
+
+$app->error(function (\Exception $exception, $code) {
+	switch ((integer) $code) {
+		
+		
+		default:
+			return 'Whoops';
+			break;
+	}
+});
+
+// Run Silex
 $app->run();
